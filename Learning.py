6f54f83b69540c59147a5e7b8c35125fd7ddadc7
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-06-29 14:06:14 +0500

    Variables and functions

--- Learning.py
+++ Learning.py
@@ -1,3 +1,5 @@
+import math
+
 print("Mubashir Learning Python..")
 print("Mubashir Learning Python..")
 print("Mubashir Learning Python..")
@@ -5,3 +7,15 @@ print("Mubashir Learning Python..")
 print("Mubashir Learning Python..")
 print("Mubashir Learning Python..")
 print("Mubashir Learning Python..")
+def PrintAverage(arr):
+    array=[]
+    array=arr
+    sum=0;
+    if array:
+        for i in array:
+            sum+=i;
+        print(math.floor(sum/ len(array)))
+
+PrintAverage([1,4,3])
+
+
